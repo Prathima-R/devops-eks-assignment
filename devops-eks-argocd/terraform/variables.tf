de27e520dc5d3029f72b9395cac19fa5f60dbc2e
Prathima-R <prathimaprati504@gmail.com>
2025-11-29 00:20:47 +0530

    Update variable descriptions and defaults in Terraform

--- devops-eks-argocd/terraform/variables.tf
+++ devops-eks-argocd/terraform/variables.tf
@@ -3,11 +3,11 @@
 variable "region" {
   description = "AWS region where resources will be deployed."
   type        = string
-  default     = "ap-south-2" # <--- IMPORTANT: Change this to your preferred AWS region (e.g., ap-south-1)
+  default     = "ap-south-2" # <-- Set to AP-SOUTH-2 (Hyderabad)
 }
 
 variable "cluster_name" {
   description = "A unique name for the EKS Cluster."
   type        = string
-  default     = "prathima-devops-eks" # <--- IMPORTANT: Change to a unique cluster name
+  default     = "prathima-devops-eks" # <-- Set to your requested cluster name
 }
