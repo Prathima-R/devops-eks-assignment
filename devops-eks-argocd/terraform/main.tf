f0668b0ce104badb753a6386d30d8c9b2ce66037
Prathima-R <prathimaprati504@gmail.com>
2025-11-29 01:01:45 +0530

    Upgrade EKS cluster Kubernetes version to 1.29

--- devops-eks-argocd/terraform/main.tf
+++ devops-eks-argocd/terraform/main.tf
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "eks_policy" {
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_master_role.arn
-  version  = "1.28" # Kubernetes version
+  version  = "1.29" # Kubernetes version
 
   vpc_config {
     subnet_ids         = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
