e00d3e3ba732a32fb854619c38df774a8726d77e
Prathima-R <prathimaprati504@gmail.com>
2025-11-29 00:18:01 +0530

    Update default AWS region and cluster name

--- devops-eks-argocd/terraform/variables.tf
+++ devops-eks-argocd/terraform/variables.tf
@@ -3,11 +3,11 @@
 variable "region" {
   description = "AWS region where resources will be deployed."
   type        = string
-  default     = "us-east-1" # <--- IMPORTANT: Change this to your preferred AWS region (e.g., ap-south-1)
+  default     = "ap-south-2" # <--- IMPORTANT: Change this to your preferred AWS region (e.g., ap-south-1)
 }
 
 variable "cluster_name" {
   description = "A unique name for the EKS Cluster."
   type        = string
-  default     = "simple-eks-cluster-dev" # <--- IMPORTANT: Change to a unique cluster name
+  default     = "prathima-devops-eks" # <--- IMPORTANT: Change to a unique cluster name
 }
