d7e055d3baa0e1510177933cf95748a9b3b48c1e
Prathima-R <prathimaprati504@gmail.com>
2025-11-29 09:25:34 +0530

    Refactor variable definitions in variables.tf

--- devops-eks-argocd/terraform/variables.tf
+++ devops-eks-argocd/terraform/variables.tf
@@ -1,13 +1,11 @@
-# variables.tf
-
 variable "region" {
-  description = "AWS region where resources will be deployed."
-  type        = string
-  default     = "ap-south-2" # <-- Set to AP-SOUTH-2 (Hyderabad)
+  description = "AWS region where resources will be deployed."
+  type        = string
+  default     = "ap-south-2"
 }
 
 variable "cluster_name" {
-  description = "A unique name for the EKS Cluster."
-  type        = string
-  default     = "prathima-devops-eks" # <-- Set to your requested cluster name
+  description = "A unique name for the EKS Cluster."
+  type        = string
+  default     = "prathima-devops-eks"
 }
