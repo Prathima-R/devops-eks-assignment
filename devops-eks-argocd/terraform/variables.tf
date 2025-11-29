c02019500fd154b47651065a2d3682b83b951227
Prathima <prathimaprati504@gmail.com>
2025-11-29 14:05:42 +0530

    Fix

--- devops-eks-argocd/terraform/variables.tf
+++ devops-eks-argocd/terraform/variables.tf
@@ -2,12 +2,12 @@ variable "cluster_name" {
   description = "The name of the EKS cluster."
   type        = string
   default     = "prathima-eks-cluster"
-}
 
 variable "ssh_key_name" {
   description = "Name of the EC2 Key Pair for SSH access to worker nodes."
   type        = string
   default     = "pratice"
 }
+
 
 
